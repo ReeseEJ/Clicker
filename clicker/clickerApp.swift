5a245a877a17b24eed72b99ef729dd5de4ad6005
ReeseEJ <121465363+ReeseEJ@users.noreply.github.com>
2025-01-22 14:01:50 -0500

    Initial Commit

---
+++ clicker/clickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickerApp.swift
+//  clicker
+//
+//  Created by Reese Jednorozec on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct clickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clickerTests/clickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  clickerTests.swift
+//  clickerTests
+//
+//  Created by Reese Jednorozec on 1/22/25.
+//
+
+import Testing
+@testable import clicker
+
+struct clickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
