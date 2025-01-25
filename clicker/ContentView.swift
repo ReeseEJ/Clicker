6379891f52ecc029532b2be3047c818240f01dc0
ReeseEJ <121465363+ReeseEJ@users.noreply.github.com>
2025-01-25 15:22:53 -0500

    Added click state

--- clicker/ContentView.swift
+++ clicker/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var clicks: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Clicks: \(clicks)")
+        Spacer()
+            Button("Click Me") {
+                clicks += 1
+            }
+        Spacer()
         }
         .padding()
     }
